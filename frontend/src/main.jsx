6872c39f2e84cca5f1b19eebfe31612fa72518b8
Hussein <almadridyramos@gmail.com>
2026-06-13 13:35:43 +0300

    Add console logs for debugging blank screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+console.log("--- SmileCare React App Initializing ---");
+console.log("Root element:", document.getElementById("root"));
+
 if (localStorage.getItem("light-mode") === "true") {
   document.body.classList.add("light-mode");
 }
